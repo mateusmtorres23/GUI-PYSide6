418b1ec5cc638ce2394be029cb2c25cbedf52d7f
Mateus Torres <mmtorres230726@gmail.com>
2026-01-07 22:50:54 -0300

    starting the calculator development: Creating the main and the main window script

--- calculator/main.py
+++ calculator/main.py
@@ -1,7 +1,16 @@
 import sys
-from PySide6.QtWidgets import QApplication, QBoxLayout, QWidget, QMainWindow
+from PySide6.QtWidgets import QApplication, QLabel
+from main_window import MainWindow
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    window = MainWindow()
 
+    label = QLabel("Text to test")
+    label.setStyleSheet("font-size: 50px;")
+    window.v_layout.addWidget(label)
+    window.adjustFixedSize()
+
+    window.show()
     app.exec()
---
+++ calculator/main_window.py
@@ -0,0 +1,16 @@
+from PySide6.QtWidgets import QVBoxLayout, QWidget, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        self.cw = QWidget()
+        self.v_layout = QVBoxLayout()
+        self.cw.setLayout(self.v_layout)
+        self.setCentralWidget(self.cw)
+
+        self.setWindowTitle("Calculadora")
+
+    def adjustFixedSize(self):    
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
